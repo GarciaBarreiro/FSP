9240163e7460c964760abb97f081db8d85a41dc2
DaniielFR <daniel.fuentes.rodriguez@rai.usc.es>
2023-10-31 09:17:00 +0100

    Subo resultados 2 y cambio script.sh con mi conf.

--- 1P/script.sh
+++ 1P/script.sh
@@ -3,8 +3,8 @@
 #SBATCH -J ex1
 #SBATCH -o ex1_%j.out
 #SBATCH -e ex1_%j.err
-#SBATCH -N 32
-#SBATCH -n 128
+#SBATCH -N 16
+#SBATCH -n 64
 #SBATCH -t 01:00:00
 #SBATCH --mem=4G
 
@@ -14,7 +14,7 @@ for nN in {1,2,4,8,16}; do
   for np in {1,2,4,8,16,32}; do
     echo $np $nN
     if [ $np -ge $nN ]; then
-      echo $np $nN
+      echo "Executing..."
       for i in {1..5}; do
         srun -N $nN -n $np $1
       done
