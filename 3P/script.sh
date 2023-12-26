986609775a951efb32d0c7125a9973ba27fbc0be
Daniel Fuentes <131860917+DaniielFR@users.noreply.github.com>
2023-12-26 21:35:19 +0100

    Update script.sh

--- 3P/script.sh
+++ 3P/script.sh
@@ -5,8 +5,8 @@
 #SBATCH -e ex1_%j.err
 #SBATCH -N 8
 #SBATCH -n 64
-#SBATCH -t 08:00:00
-#SBATCH --mem=512G
+#SBATCH -t 01:00:00
+#SBATCH --mem=100G
 
 module load cesga/2020 gcc openmpi/4.1.1_ft3
 
