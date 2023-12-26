6a0b0f325c7dad439726e250c3ddc399e112f603
Daniel Fuentes <131860917+DaniielFR@users.noreply.github.com>
2023-12-26 19:27:42 +0100

    Update script.sh

--- 3P/script.sh
+++ 3P/script.sh
@@ -10,14 +10,13 @@
 
 module load cesga/2020 gcc openmpi/4.1.1_ft3
 
-for nN in {1,2,4,8}; do
-  for np in {1,2,4,8,16,32}; do
+for nN in {2,4,8}; do
+  for np in {2,4,8,16,32}; do
     echo $np $nN
     if [ $np -ge $nN ]; then
       echo "Executing..."
-      for i in {1..5}; do
+      for i in {1..3}; do
         srun -N $nN -n $np $1 $2 $3
-        echo $1 $2 $3
       done
     fi
   done
