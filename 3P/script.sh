e25f53478c5ae9dc08cf214f3a94e4d7a09407de
Daniel Fuentes <131860917+DaniielFR@users.noreply.github.com>
2023-12-26 21:06:08 +0100

    Update script.sh

--- 3P/script.sh
+++ 3P/script.sh
@@ -6,7 +6,7 @@
 #SBATCH -N 8
 #SBATCH -n 64
 #SBATCH -t 01:00:00
-#SBATCH --mem=64G
+#SBATCH --mem=100G
 
 module load cesga/2020 gcc openmpi/4.1.1_ft3
 
@@ -18,7 +18,7 @@ for nN in {2,4,8}; do
       #for N in {50,100,1000,5000,10000,50000,100000}; do
       for N in {50000,100000}; do
         #for F in {1,2,4,8,10,20,50,1000,10000,20000}; do
-        for F in {10000}; do
+        for F in {10000,20000}; do
           echo $N $F
           for i in {1..3}; do
             srun -N $nN -n $np $1 $N $F
