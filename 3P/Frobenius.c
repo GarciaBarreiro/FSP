10c9565c930ac1757337ded08ea039e5b7ca581c
Daniel Fuentes <131860917+DaniielFR@users.noreply.github.com>
2023-12-26 18:21:42 +0100

    Update Frobenius.c

--- 3P/Frobenius.c
+++ 3P/Frobenius.c
@@ -37,24 +37,18 @@ int main(int argc, char *argv[]) {
     double overhead, total_time;
 
 
-if (argc != 3) {
+    if (argc != 3) {
         printf("Uso: mpirun -np <npes> programa <N> <F>\n");
         return EXIT_FAILURE;
     }
 
-    int N = atoi(argv[1]); // Tamaño de la matriz
-    int F = atoi(argv[2]); // Paso de la distribución cíclica
+    long N = atoi(argv[1]); // Tamaño de la matriz
+    long F = atoi(argv[2]); // Paso de la distribución cíclica
 
     if (N <= 0) {
         printf("El tamaño de a matriz debe de ser mayor que cero.\n");
         return EXIT_FAILURE;
     }
-
-    if (F > N/2) {
-        printf("El tamaño del paso es excesivo.\n");
-        return EXIT_FAILURE;
-    }
-
 
     gettimeofday(&t_prev,NULL);
     gettimeofday(&t_init,NULL);
@@ -68,8 +62,22 @@ if (argc != 3) {
     MPI_Comm_rank(MPI_COMM_WORLD, &node);
     MPI_Comm_size(MPI_COMM_WORLD, &npes);
 
+    if (npes > N) {
+        printf("Número de nodos excesivo para el tamaño de la matriz.\n");
+        printf("Reduzca el número de nodos.\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+        return 1;
+    }
+
+    if(F > N/npes) {
+        printf("El tamaño del paso es excesivo.\n");
+        printf("El tamaño máximo permitido para este caso es: %ld\n",N/npes);
+        printf("Considere reducir el paso.\n")
+        MPI_Abort(MPI_COMM_WORLD, 1);
+        return 1;
+    }
     // Proceso 0 inicializa y envía la matriz A a todos los procesos
-    if (node == 0) {
+    if (!node) {
         // Inicialización de la matriz A (aquí puedes cargarla desde un archivo o generarla)
         for (i = 0; i < N; i++) {
             for (j = 0; j < N; j++) {
@@ -78,47 +86,86 @@ if (argc != 3) {
         }
     }
 
+    // Envío el tamaño de la Matriz N a todos los procesos
+    MPI_Bcast(&N, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+
+//    if (node) {
+//        double **A = allocate_matrix(N, N);
+//    }
+
+    // Envío el paso F de la distribución cíclica a todos los procesos
+    MPI_Bcast(&F, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+
     // Envío de la matriz A a todos los procesos
-    MPI_Bcast(&A[0][0], N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    //MPI_Bcast(&A[0][0], N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    short start = 0;
+
+    if(!node) {
+        // Empieza a trabajar cuando el último nodo acaba la función allocate_matrix()
+        MPI_Recv(&start,1,MPI_SHORT,1,MPI_ANY_TAG,MPI_COMM_WORLD,NULL);
+        long fila = 0;
+
+        for (int nodo_dest = 1; nodo_dest < npes; nodo_dest++){
+            for (int i = 0; i < F; i++){
+                fila = (nodo_dest - 1) * F + i;
+                printf("%d: row = %ld\n", node, fila);
+                MPI_Send(A[fila], N, MPI_DOUBLE, nodo_dest, nodo_dest, MPI_COMM_WORLD);
+            }
+
+        }
+    } else {
+
+        double **A = allocate_matrix(F, N);
+        
+        if(node == npes-1) {
+            MPI_Send(&start, 1, MPI_SHORT, 0, 1, MPI_COMM_WORLD);
+        }
+
+        for (int i = 0; i < F; i++) {
+            MPI_Recv(A[i], N, MPI_DOUBLE, 0, node, MPI_COMM_WORLD, NULL);
+        }
+
+    }
+
 
     // Cada proceso calcula localmente la suma de los cuadrados de sus elementos asignados
-    int rows_per_proc = N / npes;
-    int start_row = node * rows_per_proc;
-    int end_row = (node + 1) * rows_per_proc;
-    if (node == npes - 1) {
-        end_row = N; // El último proceso maneja las filas restantes si N no es divisible por npes
-    }
 
-    for (i = start_row; i < end_row; i++) {
-        for (j = 0; j < N; j++) {
-            s_local += A[i][j] * A[i][j];
+    if (!node) {
+        // El número de procesos tiene un número de filas asignado que es menor
+        // que el tamaño en filas de la matriz. El nodo 0 se hará cargo de las filas
+        // restantes
+        if(F*(npes-1) < N){
+            for(int i = F*(npes-1); i < N; i ++){
+                for (j = 0; j < N; j++) {
+                    s_local += A[i][j] * A[i][j];
+                }
+            }
+        }
+    }else{
+        for (int i = 0; i < F; i++) {
+            for (j = 0; j < N; j++) {
+                s_local += A[i][j] * A[i][j];
+            }
         }
     }
-
+    
     // Reducción para obtener la suma total s en el proceso 0
-    //MPI_Reduce(&s_local, &s, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
+    MPI_Reduce(&s_local, &s, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    
+    gettimeofday(&t_final, NULL);
     overhead = (t_init.tv_sec-t_prev.tv_sec+(t_init.tv_usec-t_prev.tv_usec)/1.e6);
     total_time = (t_final.tv_sec-t_init.tv_sec+(t_final.tv_usec-t_init.tv_usec)/1.e6)-overhead;
 
     // Proceso 0 recibe las sumas locales y calcula la norma de Frobenius
     if (node == 0) {
-        double *recv_buffer = (double *)malloc(npes * sizeof(double));
-        MPI_Gather(&s, 1, MPI_DOUBLE, recv_buffer, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-        s = s_local;
-        for (i = 0; i < npes; i++) {
-            s += recv_buffer[i];
-        }
-
-        free(recv_buffer);
-
+        
         norm = sqrt(s);
 
 	printf("---------------------- RESULTS -----------------------\n");
         printf("Frobenius Norm == %.50f\n", norm);
         printf("Time == %.50f\n", total_time);
-	printf(" Rows per proc = %d\n", rows_per_proc);
+	    printf(" Rows per proc = %d\n", F);
         printf("------------------------------------------------------\n");
 
         // Create CSV file
@@ -130,7 +177,7 @@ if (argc != 3) {
         }
         setlocale(LC_ALL, "es_ES.utf8");
 
-        fprintf(fp,"%d;%.50f;%d\n", npes, total_time, rows_per_proc);
+        fprintf(fp,"%d;%.50f;%d\n", npes, total_time, F);
 
         // Closing CSV file
         fclose(fp);
