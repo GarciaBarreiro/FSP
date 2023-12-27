bcc466a8576ea5850604875feed175025a405e3d
Daniel Fuentes <131860917+DaniielFR@users.noreply.github.com>
2023-12-27 14:38:55 +0100

    Update script.sh

--- 2P/script.sh
+++ 2P/script.sh
@@ -5,23 +5,21 @@
 #SBATCH -e ex1_%j.err
 #SBATCH -N 8
 #SBATCH -n 64
-#SBATCH -t 01:00:00
-#SBATCH --mem=10G
+#SBATCH -t 08:00:00
+#SBATCH --mem=100G
 
 module load cesga/2020 gcc openmpi/4.1.1_ft3
 
 for nN in {2,4,8}; do
-  for np in {2,4,16,32}; do
+  for np in {2,4,8,16,32}; do
     echo $np $nN
     if [ $np -ge $nN ]; then
       echo "Executing..."
-      #for N in {50,100,1000,5000,10000,50000}; do
-      for N in {4,5,8}; do
-        #for F in {50,100,1000,5000,10000,50000}; do
-        for F in {4,5,8}; do
-          echo $N $F
+      for M in {50,100,1000,5000,10000,50000}; do
+        for N in {50,100,1000,5000,10000,50000}; do
+          echo $M $N
           for i in {1..3}; do
-            srun -N $nN -n $np $1 $N $F $F $N
+            srun -N $nN -n $np $1 $M $N $N $M
           done
         done
       done
