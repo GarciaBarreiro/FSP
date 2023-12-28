ceeaf1e7dc2cad5f37b029c3ce2723c08e751cc4
Daniel Fuentes <131860917+DaniielFR@users.noreply.github.com>
2023-12-28 09:44:06 +0100

    Update script.sh

--- 2P/script.sh
+++ 2P/script.sh
@@ -10,8 +10,8 @@
 
 module load cesga/2020 gcc openmpi/4.1.1_ft3
 
-for nN in {2,4,8}; do
-  for np in {2,4,8,16,32}; do
+for nN in {2}; do
+  for np in {2,4,8}; do
     echo $np $nN
     if [ $np -ge $nN ]; then
       echo "Executing..."
