cd2e3f5a1d55b69207f4d9ac1dcc0517839db0b8
Daniel Fuentes <131860917+DaniielFR@users.noreply.github.com>
2023-12-29 18:25:52 +0100

    Update 2Ptest.c

--- 2P/2Ptest.c
+++ 2P/2Ptest.c
@@ -242,7 +242,7 @@ int main(int argc, char *argv[]) {
             printf("\n");
         }
 
-        FILE *fp = fopen("res_2P-3.csv", "a");
+        FILE *fp = fopen("res_2P-4.csv", "a");
         if (!fp) {
             printf("Error opening CSV\n");
             return EXIT_FAILURE;
