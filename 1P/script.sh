4928cc88007d587787303f56497cefe05ff0c3fd
Daniel Fuentes <131860917+DaniielFR@users.noreply.github.com>
2024-01-29 09:59:18 +0100

    Update script.sh
    nada a pasado aquí

--- 1P/script.sh
+++ 1P/script.sh
@@ -8,23 +8,18 @@
 #SBATCH -t 01:00:00
 #SBATCH --mem=4G
 
+module load cesga/2020 gcc openmpi/4.1.1_ft3
 
+for nN in {1,2,4,8,16,32}; do
+  for np in {1,2,4,8,16,32}; do
+    echo $np $nN
+    if [ $np -ge $nN ]; then
+      echo "Executing..."
+      for i in {1..5}; do
+        srun -N $nN -n $np $1
+      done
+    fi
+  done
+done
 
-srun hello_world
-
-
-#module load cesga/2020 gcc openmpi/4.1.1_ft3
-
-#for nN in {1,2,4,8,16,32}; do
-#  for np in {1,2,4,8,16,32}; do
-#    echo $np $nN
-#    if [ $np -ge $nN ]; then
-#      echo "Executing..."
-#      for i in {1..5}; do
-#        srun -N $nN -n $np $1
-#      done
-#    fi
-#  done
-#done
-
-#echo "done"
+echo "done"
