0834c2602cb0f9c58ed90aacf5232dc2c95dc106
Daniel Fuentes <131860917+DaniielFR@users.noreply.github.com>
2024-01-29 09:56:08 +0100

    Update script.sh
    Creamos Script basico de ejecución de los códigos en el CESGA

--- 1P/script.sh
+++ 1P/script.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-#SBATCH -J ex1
-#SBATCH -o ex1_%j.out
-#SBATCH -e ex1_%j.err
+#SBATCH -J pae_1
+#SBATCH -o pae_1_%j.out
+#SBATCH -e pae_1_%j.err
 #SBATCH -N 32
 #SBATCH -n 256
 #SBATCH -t 01:00:00
 #SBATCH --mem=4G
 
-module load cesga/2020 gcc openmpi/4.1.1_ft3
 
-for nN in {1,2,4,8,16,32}; do
-  for np in {1,2,4,8,16,32}; do
-    echo $np $nN
-    if [ $np -ge $nN ]; then
-      echo "Executing..."
-      for i in {1..5}; do
-        srun -N $nN -n $np $1
-      done
-    fi
-  done
-done
 
-echo "done"
+srun hello_world
+
+
+#module load cesga/2020 gcc openmpi/4.1.1_ft3
+
+#for nN in {1,2,4,8,16,32}; do
+#  for np in {1,2,4,8,16,32}; do
+#    echo $np $nN
+#    if [ $np -ge $nN ]; then
+#      echo "Executing..."
+#      for i in {1..5}; do
+#        srun -N $nN -n $np $1
+#      done
+#    fi
+#  done
+#done
+
+#echo "done"
